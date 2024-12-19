0eb42b984db24798b4c259a5f4446fa2a081565a
Nathan Ferreira <nferreira1729@gmail.com>
2024-12-18 20:04:47 -0500

    feat: add new game

--- src/gameSlice.ts
+++ src/gameSlice.ts
@@ -4,7 +4,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { Game } from './models';
 
-import { GAME } from './data/AncientCivilizations';
+import { GAME } from './data/Grade7Trimester1Review';
 
 interface GameState {
   game: Game;
